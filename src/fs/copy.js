633efb89bfd291deae6b44e19ce7947c5d84e97d
Oksana Bondareva <oksanchik252@gmail.com>
2024-10-02 21:52:22 +0300

    feat: add copy.js solution

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,29 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { promises as fs } from 'fs';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const copy = async () => {
-    // Write your code here 
+    const errorMessage = 'FS operation failed';
+    try {
+        await fs.access(`${__dirname}/files`);
+        try {
+            await fs.access(`${__dirname}/files_copy`);
+            throw new Error(errorMessage);
+        } catch (error) {
+            if (error.code !== 'ENOENT') {
+                throw error;
+            }
+        }
+        await fs.cp(`${__dirname}/files`, `${__dirname}/files_copy`, { recursive: true });
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            throw new Error(errorMessage);
+        } else {
+            throw error;
+        }
+    }
 };
 
 await copy();
