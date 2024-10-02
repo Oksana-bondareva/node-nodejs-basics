0225b1b1f7ba39d96a3a64ea39197da58728c86c
Oksana Bondareva <oksanchik252@gmail.com>
2024-10-02 22:48:28 +0300

    feat: add delete.js solution

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,21 @@
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { promises as fs } from 'fs';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const remove = async () => {
-    // Write your code here 
+    const filePath = join(__dirname, 'files', 'fileToRemove.txt');
+    const errorMessage = 'FS operation failed';
+    try {
+        await fs.unlink(filePath);
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            throw new Error(errorMessage);
+        } else {
+            throw error;
+        }
+    }
 };
 
 await remove();
