1bd1f6ece977dff7edff88e99f2208fa095e1412
Oksana Bondareva <oksanchik252@gmail.com>
2024-10-03 01:44:20 +0300

    feat: add args.js solution

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv;
+    for (let i = 0; i < args.length; i++) {
+        if (args[i].startsWith('--')) {
+            const propName = args[i].replace('--', '');
+            const value = args[i + 1];
+            console.log(`${propName} is ${value}`);
+        }
+    }
 };
 
 parseArgs();
