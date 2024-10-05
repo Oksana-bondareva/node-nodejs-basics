0cabde12345dcd4710ef8b13c2e7f54c5905c0e8
Oksana Bondareva <oksanchik252@gmail.com>
2024-10-05 21:47:33 +0300

    feat: add write.js solution

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,16 @@
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { createWriteStream } from 'fs';
+import { stdin, stdout } from 'process';
+import { createInterface } from 'readline';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const file = join(__dirname, 'files', 'fileToWrite.txt');
+
 const write = async () => {
-    // Write your code here 
+    const stream = createWriteStream(file, 'utf-8');
+    stdout.write('Print your message: \n');
+    createInterface(stdin).on('line', (message) => stream.write(message + '\n'));
 };
 
 await write();
